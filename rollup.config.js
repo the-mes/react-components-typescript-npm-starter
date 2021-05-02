a625a45a286877baf9feb7dc861e1073ac53c61a
Jakub Biesiada <biesiadajakub@icloud.com>
2021-05-02 19:41:41 +0200

    docs: update

--- rollup.config.js
+++ rollup.config.js
@@ -11,6 +11,7 @@ export default {
     {
       file: pkg.main,
       format: 'cjs',
+      exports: 'default',
       sourcemap: true,
     },
     {
--- stories/index.stories.tsx
+++ stories/index.stories.tsx
@@ -19,22 +19,22 @@ export default {
   },
 } as Meta;
 
-const DemoBlock = styled.div`
-  background-color: #a19f9d;
-  color: #fff;
-  font-weight: 600;
-  height: 40px;
-  line-height: 40px;
-  margin-bottom: 16px;
-  text-align: center;
-`;
+// const DemoBlock = styled.div`
+//   background-color: #a19f9d;
+//   color: #fff;
+//   font-weight: 600;
+//   height: 40px;
+//   line-height: 40px;
+//   margin-bottom: 16px;
+//   text-align: center;
+// `;
 
-export const ExampleStory: Story<Props> = ({ dir }) => <DemoBlock></DemoBlock>;
+// export const ExampleStory: Story<Props> = ({ dir }) => <DemoBlock></DemoBlock>;
 
-ExampleStory.storyName = 'Example Story';
+// ExampleStory.storyName = 'Example Story';
 
-ExampleStory.parameters = {
-  controls: {
-    // disabled: true,
-  },
-};
+// ExampleStory.parameters = {
+//   controls: {
+//     // disabled: true,
+//   },
+// };
